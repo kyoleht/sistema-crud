3250d45eb031b44ea4a780e829229cca568eb926
kyoleht <rafaelferreiradasilva1812@gmail.com>
2024-11-22 10:13:42 -0300

    Criação e Estilização do Botão de Listar Clientes

--- insertC.php
+++ insertC.php
@@ -12,20 +12,22 @@
         <form action="php_action/insertC.php" method="POST" class="form-cadastro">
             <div class="form-group">
                 <label for="nome">Nome: </label>
-                <input type="text" id="nome" name="nome" required>
+                <input type="text" id="nome" name="nome" placeholder="Insira seu Nome" required>
             </div>
 
             <div class="form-group">
                 <label for="email">Email: </label>
-                <input type="text" id="email" name="email" required>
+                <input type="text" id="email" name="email" placeholder="Insira seu Email" required>
             </div>
 
             <div class="form-group">
                 <label for="cidade">Cidade: </label>
-                <input type="text" id="cidade" name="cidade" required>
+                <input type="text" id="cidade" name="cidade" placeholder="Insira sua Cidade" required>
             </div>
 
             <button type="submit" class="btn">Cadastrar Cliente</button>
+            
+            <a class="botao-listar" href="index.php">Listar Clientes</a>
         </form>
     </div>
 </body>
